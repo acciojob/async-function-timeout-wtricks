5dc799aa7fcd55b9f8db8082ef206cccb2ae403d
Anuj Kumar <101552403+wtricks@users.noreply.github.com>
2023-08-25 09:00:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+document.getElementById("btn").onclick = async () => {
+	const res = await new Promise((res) => {
+		setTimeout(res, document.getElementById("delay").value, document.getElementById("text").value);
+	})
+
+	document.getElementById("output").textContent = res;
+}
